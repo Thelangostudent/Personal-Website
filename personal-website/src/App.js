7866051fa2a39b275a5d8a351ceb177a6656a99d
Thelango99 <sanderrf@stud.ntnu.no>
2022-08-19 19:24:58 +0200

    BrowserRouter and navbar initial implementation. This is it for today, see you next commit.

--- personal-website/src/App.js
+++ personal-website/src/App.js
@@ -1,23 +1,32 @@
 import logo from './logo.svg';
-import './cssFiles/App.css';
+//import './cssFiles/App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import NavBar from './navBar/navBar/';
+import LandingPage from './landingPage/landingPage';
+import ProjectsPages from './projectsPages/baseProjectPage';
+import AboutPage from './aboutPage/aboutPage';
+import JobsPage from './jobsPage/baseJobPage';
+import QualificationsPage from './qualificationsPage/baseQualificationsPage';
+
+
+/***
+ * Acts as BrowserRouter for this site (as per usual in a React project).
+ * */
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          PlaceHolder!! Website coming soon.
-        </a>
-      </header>
+      <div className="App">
+          <BrowserRouter>
+              <Routes>
+                  <Route path="/" element={<NavBar />}>
+                      <Route index element={<LandingPage />} />
+                      <Route path="baseProjectPage" element={<ProjectsPages />} />
+                      <Route path="aboutPage" element={<AboutPage />} />
+                      <Route path="baseJobPage" element={<JobsPage />} />
+                      <Route path="baseQualificationsPage" element={<QualificationsPage />} />
+                  </Route>
+              </Routes>
+          </BrowserRouter>
     </div>
   );
 }
--- personal-website/src/navBar/navBar.js
+++ personal-website/src/navBar/navBar.js
@@ -1,1 +1,54 @@
 // JavaScript source code
+/**
+ This is the navbar to be present as a module in all pages.
+
+ Author: Sander Ringmo Fylkesnes
+
+ Last updated: see file date.
+ 
+ */
+
+import { Outlet, Link } from 'react-router-dom';
+
+
+function navBar() {
+    return (
+        <div className="navBar">
+            <nav id = "naviGation">
+              
+                <ul id="navBarList">
+                    <li className="navItem">
+                        <Link to ="/"> Logo goes here (landing page)</Link>
+                    </li>
+                    <li className="navItem">
+                        <Link to ="/aboutPage">About</Link>
+                    </li>
+                    <li className="navItem">
+                        <Link to ="/baseProjectPage">Projects</Link>
+                    </li>
+                    <li className="navItem">
+                        <Link to ="/baseQualificationsPage">Qualifications</Link>
+                    </li>
+                    <li className="navItem">
+                        <Link to ="/baseJobPage" >Jobs</Link>
+                    </li>
+
+                </ul>
+
+
+            </nav>
+            <Outlet/>
+
+        </div>
+
+      
+                
+        
+        );
+
+
+}
+
+export default navBar;
+
+
