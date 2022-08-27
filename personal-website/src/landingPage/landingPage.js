05753afd95eacf7e398c4eada27319306a18d6e1
Thelango99 <sanderrf@stud.ntnu.no>
2022-08-27 04:54:45 +0200

    Initial landing page structure written. CSS need to be made for it. Not sure if all features will meet deadline.

--- personal-website/src/landingPage/landingPage.js
+++ personal-website/src/landingPage/landingPage.js
@@ -1,12 +1,38 @@
 // JavaScript source code
 
+// consider changing newscontent into individual cards instead.
+
+/***
+ * This js file is the main landing page.
+ * Here will news about me and the site be.
+ * */
+
 function landingPage() {
     return (
+        <div id = "landingPageContainer">
+        <h2 id="pageHeader">Main Page</h2>
+        
+        <div id="inPageshortCuts">
+                <ul> Shortcuts
+                    <li className="shortCut"></li>
 
-        <p>Landing page test</p>
-        
-        
-        
+
+                </ul> 
+            </div>
+
+            <div id="newsContent">
+                <div id="news_1">
+                    <h3 className="newsHeadline"> First Version Finally launched!</h3>
+                    <p className="newsContent">The website has now launched in its initial form.
+                        I know it is rough right now, so feedback at this stage is appreciated.
+                        On this site you will find information about me and my projects and any relevant news.
+                        if you wish to contact me, check the about section.</p>
+                </div>
+
+
+            </div>
+
+        </div>
         
     );
 
