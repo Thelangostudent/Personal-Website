15b428284f4b7ba6d509d096e269ff8288bbe5c7
Thelango99 <sanderrf@stud.ntnu.no>
2022-10-09 17:10:17 +0200

    Implemented the dark/light mode. A more beatuiful implementation might be needed later (e.g only one button).

--- personal-website/src/darkLightButton/darklightButton.js
+++ personal-website/src/darkLightButton/darklightButton.js
@@ -1,4 +1,4 @@
-// JavaScript source code
+
 
 
 
@@ -7,8 +7,8 @@ function darkLightButton() {
 
     return (
         <div>
-        <button type="button" id="darkButton">Dark</button>
-        <button type="button" id="lightButton">Light</button>
+            <button type="button" value="dark" id="darkButton" onClick={e => darkOrLightMode(e.target.value)}>Dark</button>
+            <button type="button" value="light" id="lightButton" onClick={e => darkOrLightMode(e.target.value)}>Light</button>
         
         </div >
         
@@ -18,14 +18,32 @@ function darkLightButton() {
         );
 
 
+    function darkOrLightMode(darkOrLight) {
 
+        
+       
+        var textColour = document.getElementById("mainBody");
+
+        if (darkOrLight === "dark") {
+
+            textColour.style.color = "white";
+            textColour.style.backgroundColor = "black";
+
+        }
+
+        if (darkOrLight === "light") {
+
+            textColour.style.color = "black";
+            textColour.style.backgroundColor = "antiquewhite";
+
+        }
+
+
+    }
 
 }
+
+
 
 export default darkLightButton;
 
-function darkOrLightMode() {
-
-
-
-}
